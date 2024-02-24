164e36b14363364c5633022be98232737507526c
Genrikh Galachyan <galachyan2019@gmail.com>
2024-02-24 20:02:34 +0300

    add lesson3: task1

---
+++ lesson3/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numerator = a * b;
+    int denomerator = c + d;
+    double result = numerator / denomerator;
+    return result;
+}
+
+CalculateFormula(1, 2, 3, 4);
+
+double result = CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(result);
